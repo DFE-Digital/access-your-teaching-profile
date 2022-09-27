679d2d089bf274c23db3cbca3255461f2c298e56
Merril Mathew <merril.mathew@outlook.com>
2022-09-27 15:12:35 +0100

    Add Terraform configuration
    Access Your Teaching Profile deployment is done through Terraform. This
    enables idempotent deployment of Azure resources with a state. This
    commit adds the files to deploy to Azure.

---
+++ terraform/output.tf
@@ -0,0 +1,14 @@
+output "app_fqdn" {
+  value = azurerm_linux_web_app.aytp-app.default_hostname
+}
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.postgres-server.name
+}
+
+output "blue_green" {
+  value = var.enable_blue_green
+}
+
+output "web_app_slot_name" {
+  value = local.web_app_slot_name
+}
