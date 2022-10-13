f9575f3644ce0f46d0ee127d89288b5c7306d608
Steve Laing <steve.laing@gmail.com>
2022-10-13 16:50:50 +0100

    Add okcomputer Sidekiq healthcheck

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -3,6 +3,13 @@ OkComputer.mount_at = "health"
 
 OkComputer::Registry.register "postgresql", OkComputer::ActiveRecordCheck.new
 
+Sidekiq.redis do |conn|
+  OkComputer::Registry.register "sidekiq",
+                                OkComputer::RedisCheck.new(conn.connection)
+  OkComputer::Registry.register "sidekiq-latency",
+                                OkComputer::SidekiqLatencyCheck.new("default")
+end
+
 OkComputer::Registry.register "version", OkComputer::AppVersionCheck.new
 
 OkComputer.make_optional %w[version]
